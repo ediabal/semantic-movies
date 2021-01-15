ca8fabcb6b25e8b2bb312af3977466399e8f3a76
Logan Wholey <logan.wholey@fireeye.com>
2021-01-15 12:11:17 -0800

    adding omdb api key and env file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('hey', process.env.REACT_APP_OMDB_API)
   return (
     <div className="App">
       <header className="App-header">
